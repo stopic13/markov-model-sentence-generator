8f0586c369e2695ae9162148a3a97f83ed4c15ab
stopic13 <sara.topic.18@dartmouth.edu>
2018-04-11 17:05:55 -0400

    some files i forgot to add before

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ $('#main').html('Here we go!');
 
 const Timer = {
   secondsPassed: 0,
-  consoleMsg() {
+  increment() {
     this.secondsPassed += 1;
     $('#main').text(`You've been on this page for ${this.secondsPassed} seconds!`);
   },
-  increment() {
-    setInterval(() => { this.consoleMsg(); }, 1000);
+  trackTime() {
+    setInterval(() => { this.increment(); }, 1000);
   },
 };
-Timer.increment();
+Timer.trackTime();
