9a37f89cf580d5e8b1c478b411c0497133eeda9f
stopic13 <sara.topic.18@dartmouth.edu>
2018-04-11 10:50:01 -0400

    progress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('starting up');
+// change require to es6 import style
+import $ from 'jquery';
+$('#main').html('Here we go!');
+
+
+let Timer = {
+  secondsPassed: 0,
+  consoleMsg: function(){
+    this.secondsPassed++;
+    $(`#main`).text(`You've been on this page for ${this.secondsPassed} seconds!`);
+  },
+   increment: function(){
+     setInterval(() =>
+     {this.consoleMsg()}, 1000);
+   }
+}
+Timer.increment();
