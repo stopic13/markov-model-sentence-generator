202b4a987b808898209d60f76c8bc54a3e69e34b
stopic13 <sara.topic.18@dartmouth.edu>
2018-12-21 23:29:05 -0500

    adding some more styles, some more books, and working on explaining

--- src/components/app.js
+++ src/components/app.js
@@ -5,10 +5,18 @@ import { Jumbotron, Button } from 'react-bootstrap';
 import axios from 'axios';
 import './style.scss';
 import './pride_and_prejudice.jpg';
+import './jane_eyre.jpg';
+import './the_jungle.jpg';
+import './a_little_princess.jpg';
+import './madame_bovary.jpg';
 
 const AVAILABLE_BOOKS = [
   { title: 'Pride and Prejudice', author: 'Jane Austen', img: 'pride_and_prejudice.jpg' },
-  { title: 'Jane Eyre', author: 'Charlotte Brontë', img: 'pride_and_prejudice.jpg' },
+  { title: 'Jane Eyre', author: 'Charlotte Brontë', img: 'jane_eyre.jpg' },
+  { title: 'The Jungle', author: 'Upton Sinclair', img: 'the_jungle.jpg' },
+  { title: 'A Little Princess', author: 'Frances Hodgson Burnett', img: 'a_little_princess.jpg' },
+  { title: 'Madame Bovary', author: 'Gustave Flaubert', img: 'madame_bovary.jpg' },
+
 ];
 
 class SentenceGenerator extends React.Component {
@@ -16,10 +24,13 @@ class SentenceGenerator extends React.Component {
     super(props);
     // Don't call this.setState() here!
     this.state = {
-      sentence: 'Click the button below to generate a sentence',
+      sentence: ' \n \n \n',
       books: {
         'Pride and Prejudice': false,
         'Jane Eyre': false,
+        'The Jungle': false,
+        'A Little Princess': false,
+        'Madame Bovary': false,
       },
     };
     this.handleClick = this.handleClick.bind(this);
@@ -66,6 +77,7 @@ class SentenceGenerator extends React.Component {
             onClick={e => this.selectBook(e, book.title)}
             value={book.title}
           >
+            <div className="checkbox"><i className="fas fa-check" /></div>
             <img src={book.img} alt={book.title} />
             <h1> {book.title} </h1>
             <p> {book.author} </p>
@@ -80,6 +92,7 @@ class SentenceGenerator extends React.Component {
             onClick={e => this.selectBook(e, book.title)}
             value={book.title}
           >
+            <div className="checkbox" />
             <img src={book.img} alt={book.title} />
             <h1> {book.title} </h1>
             <p> {book.author} </p>
@@ -99,7 +112,7 @@ class SentenceGenerator extends React.Component {
     console.log(this.state);
     return (
       <div className="main-container">
-        <h1>Sentence Generator</h1>
+        <h1 className="title">Second-Order Markov Chain Sentence Generator</h1>
         <div className="flex-container-wrapper">
           <div className="sentence-box">
             <Jumbotron className="jumbo">
@@ -107,7 +120,7 @@ class SentenceGenerator extends React.Component {
                 {this.state.sentence}
               </p>
               <p>
-                <Button className="submit-button" bsStyle="primary" onClick={this.handleClick}> Generate Sentence </Button>
+                <Button className="submit-button" bsStyle="primary" onClick={this.handleClick}> Go! </Button>
               </p>
             </Jumbotron>
           </div>
@@ -118,8 +131,37 @@ class SentenceGenerator extends React.Component {
             {this.renderBooks()}
           </div>
         </div>
-
-        <Footer />
+        <div className="flex-container-wrapper">
+          <h1> About </h1>
+          <div className="sentence-box">
+            <h3> Q: What is a Markov Chain </h3>
+            <h4> A mathematical model describing the probabilty of a transitioning to a set of states, based only on the current state. </h4>
+            <h3> Q: What does `Second-Order` mean? </h3>
+            <h4> It means the probabilty of transitioning to the next state is based on the previous two states.
+             In the context of sentence generation it means the state space consists of two words.
+            </h4>
+            <h3> Q: How are the probabilties generated? </h3>
+            <h4> The selected texts are parsed into a dictionary where the key is a an ordered pair of <span style={{ color: 'blue' }}>(word_1, word_2)</span> and the value
+            is another dictionary where the key is <span style={{ color: 'green' }}> word_3 </span> and the value is the frequency of
+              <span style={{ color: 'green' }}> word_3 </span>ocurring after <span style={{ color: 'blue' }}>word_1</span>.
+            and <span style={{ color: 'blue' }}> word_2 </span>. Then, for each ordered pair of words in the dictonary the corresponding
+             dictionary is turned into a mapping of word_3 to the probabiillity of
+             word_3 ocurring after word_1 and word_2 by dividing the frequency of word_3 by the total frequencies of all the words ocurring after
+              <span style={{ color: 'blue' }}>word_1</span> and <span style={{ color: 'blue' }}>word_2</span>..
+            </h4>
+            <h3> Q: How are the sentences generated? </h3>
+            <h4> A pair <span style={{ color: 'blue' }}>(word_1, word_2)</span> where <span style={{ color: 'blue' }}>word_1</span>
+             has a capital letter is chosen at random. Based on <span style={{ color: 'blue' }}>(word_1, word_2)</span>,,
+             word_3 is chosen according to the probabilty of it occurring after word_1 and word_2.
+             This process repeats until a the model trasitions to a state where word_2 has stop punctuation (., !, or ?).
+            </h4>
+            <h3> Q: Where is the source available? </h3>
+            <h4>
+              Frontend: <a href="http://www.github.com/stopic13/markov-model-sentence-generator"><i class="fa fa-github" /></a>
+              <br /> Backend:  <a href="http://www.github.com/stopic13/markov-model-sentence-generator-backend"><i class="fa fa-github" /></a>
+            </h4>
+          </div>
+        </div>
       </div>
     );
   }
@@ -130,9 +172,6 @@ const FallBack = () => {
   return <div>URL Not Found</div>;
 };
 
-const Footer = () => {
-  return <footer> <a href="http://www.github.com/stopic13/markov-model-sentence-generator"> <i className="fab fa-github" /> </a></footer>;
-};
 
 const App = () => {
   return (
